465fa07f0ba0a35355ffcc537623743ffeba46df
gustav-hisker <gustav.hisker@outlook.de>
2025-11-06 12:58:47 +0100

    .

--- main.py
+++ main.py
@@ -388,4 +388,4 @@ def tournament(n: int = 5, k: int = 5, w: int = 20):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
