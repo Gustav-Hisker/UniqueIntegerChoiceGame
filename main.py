eb1050c10235dcdd23200e95edfa89579e9f09d3
Lutz Pfannenschmidt <lutz@lutz-pfannenschmidt.de>
2025-12-06 15:54:36 +0100

    change port

--- main.py
+++ main.py
@@ -482,6 +482,6 @@ async def admin():
         return f.read()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="127.0.0.1", port=8080)
 else:
     print(f"The admin password is: {adminPW}")
