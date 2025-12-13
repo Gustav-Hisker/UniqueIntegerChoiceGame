cf75e089de05bfcdafa0e04228a330ef0a3eec6d
gustav-hisker <gustav.hisker@outlook.de>
2025-12-13 09:31:17 +0100

    changed port to 80

--- main.py
+++ main.py
@@ -596,6 +596,6 @@ async def getTeams():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080)
+    uvicorn.run("main:app", host="127.0.0.1", port=80)
 else:
     print(f"The admin password is: {adminPW}")
